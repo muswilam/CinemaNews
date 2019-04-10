755cd6d8d11b9e4d518013a1128472d448a2cba5
Sully <jackswilam@gmail.com>
2019-04-10 20:08:52 +0200

    view list off actors with Entity Framework to get data from DB and customize some properties attributes

---
+++ CinemaNews/Controllers/ActorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CinemaNews.Models;
+
+namespace CinemaNews.Controllers
+{
+    public class ActorController : Controller
+    {
+        private DBHollywoodContext db = new DBHollywoodContext();
+        public ActionResult List()
+        {
+            return View(db.Actors.ToList());
+        }
+    }
+}
---
+++ CinemaNews/Models/Actors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CinemaNews.Models
+{
+    [MetadataType(typeof(ActorMetadata))]
+    public partial class Actor
+    {
+    }
+    //customize Properties 
+    public class ActorMetadata
+    {
+        [DataType(DataType.Date)]
+        [DisplayName("Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+
+        [DataType(DataType.Url)]
+        [DisplayName("IMDb Profile")]
+        public string IMDb { get; set; }
+
+        [DataType(DataType.Url)]
+        [DisplayName("Instagram Profile")]
+        public string Profile { get; set; }
+    }
+}
